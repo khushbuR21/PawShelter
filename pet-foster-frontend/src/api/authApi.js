eaf92d5a27a65b4c6f5a4e2dd3a82ba5c34bd6eb
Khushbu Rathi <71555128+khushbuR21@users.noreply.github.com>
2026-02-04 13:10:32 +0530

    Delete pet-foster-frontend directory

--- pet-foster-frontend/src/api/authApi.js
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "./axiosConfig";
-
-export const loginUser = (loginData) => {
-  return axiosInstance.post("/auth/login", loginData);
-};
-
-export const registerUser = (registerData) => {
-  return axiosInstance.post("/auth/register", registerData);
-};
